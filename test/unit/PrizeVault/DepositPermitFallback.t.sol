4b3a759aad3ff897f12e3e3dec12fb66fe918bf0
Trevor Richard <trmidpoint@gmail.com>
2024-01-19 16:43:48 +0000

    add deposit permit fallback test

---
+++ test/unit/PrizeVault/DepositPermitFallback.t.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { IERC4626 } from "openzeppelin/token/ERC20/extensions/ERC4626.sol";
+
+import { IERC20, UnitBaseSetup, PrizeVault } from "./UnitBaseSetup.t.sol";
+
+import { ERC20PermitFallbackMock } from "../../contracts/mock/ERC20PermitFallbackMock.sol";
+import { ERC20PermitMock } from "../../contracts/mock/ERC20PermitMock.sol";
+
+contract PrizeVaultDepositPermitFallbackTest is UnitBaseSetup {
+
+    /* ============ setup ============ */
+
+    function setUpUnderlyingAsset() public override returns (ERC20PermitMock) {
+        return ERC20PermitMock(address(new ERC20PermitFallbackMock()));
+    }
+
+    /* ============ tests ============ */
+
+    function testDepositWithoutPermit_AllowanceNotSet() external {
+        vm.startPrank(alice);
+
+        uint256 _amount = 1000e18;
+        underlyingAsset.mint(alice, _amount);
+
+        underlyingAsset.approve(address(vault), _amount * 2);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                PrizeVault.PermitAllowanceNotSet.selector,
+                alice,
+                address(vault),
+                _amount,
+                _amount * 2
+            )
+        );
+
+        uint8 _v = 0;
+        bytes32 _r = bytes32(0);
+        bytes32 _s = bytes32(0);
+        vault.depositWithPermit(_amount, alice, block.timestamp, _v, _r, _s);
+
+        vm.stopPrank();
+    }
+
+    function testForceDepositWithoutPermitByThirdParty() external {
+        vm.startPrank(alice);
+
+        uint256 _amount = 1000e18;
+        underlyingAsset.mint(alice, _amount);
+
+        underlyingAsset.approve(address(vault), _amount);
+
+        vm.stopPrank();
+
+        vm.expectRevert(
+            abi.encodeWithSelector(PrizeVault.PermitCallerNotOwner.selector, address(this), alice)
+        );
+
+        uint8 _v = 0;
+        bytes32 _r = bytes32(0);
+        bytes32 _s = bytes32(0);
+        vault.depositWithPermit(_amount, alice, block.timestamp, _v, _r, _s);
+    }
+
+    function testFailPermitSign() external {
+        vm.startPrank(alice);
+
+        uint256 _amount = 1000e18;
+        underlyingAsset.mint(alice, _amount);
+
+        (uint8 _v, bytes32 _r, bytes32 _s) = _signPermit(
+            underlyingAsset,
+            vault,
+            _amount,
+            alice,
+            alicePrivateKey
+        );
+
+        vm.stopPrank();
+    }
+}
