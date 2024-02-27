6d7dd62d64de8efab8e6b8665247816ead64d423
Trevor Richard <trmidpoint@gmail.com>
2024-02-27 17:10:29 +0000

    add sfrxETH integration test

---
+++ test/integration/frax/sfrxEthEthereum.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { BaseIntegration, IERC20, IERC4626 } from "../BaseIntegration.t.sol";
+
+contract sfrxEthEthereumIntegrationTest is BaseIntegration {
+    uint256 fork;
+    uint256 forkBlock = 19284702;
+    uint256 forkBlockTimestamp = 1708623756;
+
+    address internal _asset = address(0x5E8422345238F34275888049021821E8E08CAa1f);
+    address internal _assetWhale = address(0x9c3B46C0Ceb5B9e304FCd6D88Fc50f7DD24B31Bc);
+    address internal _yieldVault = address(0xac3E018457B222d93114458476f3E3416Abbe38F);
+
+    /* ============ setup ============ */
+
+    function setUpUnderlyingAsset() public virtual override returns (IERC20 asset, uint8 decimals, uint256 approxAssetUsdExchangeRate) {
+        return (IERC20(_asset), 18, 3000e18); // pegged to ETH value
+    }
+
+    function setUpYieldVault() public virtual override returns (IERC4626) {
+        return IERC4626(_yieldVault);
+    }
+
+    function setUpFork() public virtual override {
+        fork = vm.createFork(vm.rpcUrl("mainnet"), forkBlock);
+        vm.selectFork(fork);
+        vm.warp(forkBlockTimestamp);
+    }
+
+    function beforeSetup() public virtual override {
+        lowGasPriceEstimate = 7 gwei;
+    }
+
+    function afterSetup() public virtual override { }
+
+    /* ============ helpers to override ============ */
+
+    /// @dev The max amount of assets than can be dealt.
+    function maxDeal() public virtual override returns (uint256) {
+        return underlyingAsset.balanceOf(_assetWhale);
+    }
+
+    /// @dev May revert if the amount requested exceeds the amount available to deal.
+    function dealAssets(address to, uint256 amount) public virtual override prankception(_assetWhale) {
+        underlyingAsset.transfer(to, amount);
+    }
+
+    /// @dev Yield accrues over time
+    function _accrueYield() internal virtual override {
+        vm.warp(block.timestamp + 1 days);
+    }
+
+    /// @dev Yield is algorithmically controlled and cannot be inverted, so we'll simulate loss in the prize vault instead of the yield vault.
+    function _simulateLoss() internal virtual override prankception(address(prizeVault)) {
+        yieldVault.transfer(_assetWhale, yieldVault.balanceOf(address(prizeVault)) / 2);
+    }
+
+}
